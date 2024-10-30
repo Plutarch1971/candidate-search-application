87e18b7837e5805fbfd6cedd175bf910ef8d59df
Matt-Verni <matthewpmendez@gmail.com>
2024-10-30 15:17:44 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT || '3000')
+  },
+  preview: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: true // needed for Docker/container environments
+  }
 });
+
